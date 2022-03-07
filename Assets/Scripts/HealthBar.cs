05c35ffea4e6226bf977dcbb449e5af99fff5016
Ренат <syltan.100renrgen@gmail.com>
2022-03-07 23:09:33 +0300

    add helath bar

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private Slider _slider;
+
+    private void OnEnable()
+    {
+        _player.HealthChanged += OnHealthChanged;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthChanged -= OnHealthChanged;
+    }
+
+    private void Start()
+    {
+        _slider.maxValue = _player.MaxHealth;
+        _slider.value = _player.MaxHealth;
+    }
+
+    private void OnHealthChanged()
+    {
+        _slider.value = _player.CurrentHealth;
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,23 +1,39 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 [RequireComponent(typeof(Animator))]
 [RequireComponent(typeof(Weapon))]
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private int _health;
+    [SerializeField] private int _maxHealth;
     [SerializeField] private float _secondBetweenShoot;
 
     private Weapon _weapon;
     private float _lastShootTime;
     private Animator _animator;
+    private int _currentHealth;
+
+    public event UnityAction HealthChanged;
+
+    public int CurrentHealth => _currentHealth;
+    public int MaxHealth => _maxHealth;
+
+    private void OnValidate()
+    {
+        if (_maxHealth <= 0)
+        {
+            _maxHealth = 50;
+        }
+    }
 
     private void Start()
     {
         _animator = GetComponent<Animator>();
         _weapon = GetComponent<Weapon>();
+        _currentHealth = _maxHealth;
     }
 
     private void Update()
@@ -34,9 +50,10 @@ public class Player : MonoBehaviour
 
     public void TakeDamage(int damage)
     {
-        _health -= damage;
+        _currentHealth -= damage;
+        HealthChanged?.Invoke();
 
-        if (_health <= 0)
+        if (_currentHealth <= 0)
         {
             Debug.Log("ТЫ УМЕР!");
         }
